79de08db40543761602babf2e2fe06064899375a
wdhysrz <2918159660@qq.com>
2026-06-03 08:16:45 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,4 +2,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i '/CONFIG_PACKAGE_lutil=y/d' .config
+echo "# CONFIG_PACKAGE_lutil=y" >> .config
 
