da8bb1e08a0da68b08e69c4d91ef43df57ec3a38
wdhysrz <2918159660@qq.com>
2026-06-03 15:17:43 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sed -i 's/PKG_INSTALL:=1/PKG_INSTALL:=1\nPKG_REPLACE:=l1util/' package/mtwifi-cfg/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
