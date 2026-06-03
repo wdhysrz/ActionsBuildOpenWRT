91a5f513a4891edf902ca50f1aa869544b1132d0
wdhysrz <2918159660@qq.com>
2026-06-03 10:01:23 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,7 +5,9 @@
 
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
-#sed -i '$a src-git passwall # # https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
+sed -i '$a src-git passwall # # https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
+sed -i '/CONFIG_PACKAGE_lutil=y/d' .config
+echo "# CONFIG_PACKAGE_lutil=y" >> .config
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
