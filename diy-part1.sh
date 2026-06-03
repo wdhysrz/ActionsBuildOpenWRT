73d9a8818b52bd33c2c83c311b412ec4e073813e
wdhysrz <2918159660@qq.com>
2026-06-03 14:41:38 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,7 +6,8 @@
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 #sed -i '$a src-git passwall # # https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-sed -i 's/PKG_INSTALL:=1/PKG_INSTALL:=1\nCONFLICTS:=l1util/' package/mtwifi-cfg/Makefile
+sed -i 's/PKG_INSTALL:=1/PKG_INSTALL:=1\nPKG_REPLACE:=l1util/' package/mtwifi-cfg/Makefile
+mtwifi-cfg/Makefile
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
