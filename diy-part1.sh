fbbed0a0867963d7f071901604411b265c8ad14d
wdhysrz <2918159660@qq.com>
2026-06-03 12:12:50 +0800

    更新 diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,6 +8,7 @@
 #sed -i '$a src-git passwall # # https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
 sed -i '/CONFIG_PACKAGE_lutil=y/d' .config
 echo "# CONFIG_PACKAGE_lutil=y" >> .config
+rm -f staging_dir/target-aarch64_cortex-a53_musl/sbin/lutil
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
