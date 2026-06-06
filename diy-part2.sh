891d23d808dfdbaf198ee83c1a8611c110eec4ae
wdhysrz <2918159660@qq.com>
2026-06-06 18:30:51 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 修正 360T7 设备树中错误的包含文件名
+if [ -f "target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts" ]; then
+    sed -i 's/"mt7981.dtsi"/"mt7981b.dtsi"/g' target/linux/mediatek/dts/mt7981b-qihoo-360t7.dts
+    echo "成功将 360T7 设备树中的 mt7981.dtsi 修正为 mt7981b.dtsi！"
+fi
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
